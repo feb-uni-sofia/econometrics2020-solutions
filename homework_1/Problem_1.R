6317f85216b4f28265d3bf06a99cd4f8c196faab
Boyko Amarov <boyko.amarov@gmail.com>
2020-03-23 18:29:34 +0200

    Create Problem_1.R

---
+++ homework_1/Problem_1.R
@@ -0,0 +1,23 @@
+## Homework 1, problem 1
+
+## a)
+
+patients <- read.csv2('https://firebasestorage.googleapis.com/v0/b/uni-sofia.appspot.com/o/data%2FCOVID19_2020_open_line_list%20-%20outside_Hubei_subset.csv?alt=media&token=919a181b-6417-45fd-9314-91497bb4b0fc', stringsAsFactors = FALSE)
+
+## b)
+
+## c)
+
+## d)
+
+## e)
+
+## f)
+
+## g)
+
+## h)
+
+## i)
+
+## j)
