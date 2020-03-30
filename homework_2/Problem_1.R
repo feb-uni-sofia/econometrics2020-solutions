c593427a479ef47ca4aba253b0da148a04e25044
Boyko Amarov <boyko.amarov@gmail.com>
2020-03-30 13:51:09 +0300

    Homework 2: add links to homework problem and classroom invitation to starter code.

--- homework_2/Problem_1.R
+++ homework_2/Problem_1.R
@@ -1,3 +1,10 @@
+###########################################################################################################
+## Econometrics 2020
+## Homework 2
+## Homework assignment text: https://feb-uni-sofia.github.io/econometrics2020-solutions/homeworks.html#homework-2
+## Homework classroom invitation: https://classroom.github.com/g/qogtEQ5b
+###########################################################################################################
+
 ## 1) Download and read data
 
 linton <- read.csv('https://firebasestorage.googleapis.com/v0/b/uni-sofia.appspot.com/o/data%2Flinton2020_complete_obs.csv?alt=media&token=1ba3e0d4-658b-4639-ae5b-408e9e109047', stringsAsFactors = FALSE)
