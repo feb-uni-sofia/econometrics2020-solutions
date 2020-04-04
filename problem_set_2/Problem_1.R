5da4542b473d6c269145b097d3dd1519e2340c30
Boyko Amarov <boyko.amarov@gmail.com>
2020-04-04 19:10:22 +0300

    Create Problem_1.R

---
+++ problem_set_2/Problem_1.R
@@ -0,0 +1,23 @@
+###########################################################################################################
+## Econometrics 2020
+## Homework 2
+## Homework assignment text: https://feb-uni-sofia.github.io/econometrics2020-solutions/assignments.html#assignment-2
+###########################################################################################################
+
+## 1) Download and read data
+
+linton <- read.csv('https://firebasestorage.googleapis.com/v0/b/uni-sofia.appspot.com/o/data%2Flinton2020_complete_obs.csv?alt=media&token=1ba3e0d4-658b-4639-ae5b-408e9e109047', stringsAsFactors = FALSE)
+
+## 2)
+
+## 3)
+
+## 4)
+
+## 5)
+
+## 6)
+
+## 7)
+
+## 8)
